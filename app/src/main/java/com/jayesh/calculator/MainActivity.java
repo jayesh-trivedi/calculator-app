f6433016c01edf769ed4db1ac1c0e9425246991e
jayesh-trivedi <jayesh.trvdi@gmail.com>
2021-05-21 20:11:53 +0530

    changed functionality in accordance to UI

--- app/src/main/java/com/jayesh/calculator/MainActivity.java
+++ app/src/main/java/com/jayesh/calculator/MainActivity.java
@@ -3,97 +3,181 @@ package com.jayesh.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    double answer = 0;
-    double num1,num2;
-    String operation = "";
+    private int[] numericButtons = {R.id.btnZero, R.id.btnOne, R.id.btnTwo, R.id.btnThree, R.id.btnFour, R.id.btnFive, R.id.btnSix, R.id.btnSeven, R.id.btnEight, R.id.btnNine, R.id.btnDot};
+    private int[] operatorButtons = {R.id.btnAdd, R.id.btnSubtract, R.id.btnMultiply, R.id.btnDivide};
+    private int [] keyButtons = { R.id.btnClear, R.id.btnDelete, R.id.btnEqual};
+    private double operand1,operand2;
+    private Boolean operatorSelected = false;
+    String result = "";
+    TextView txtScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText input1 = findViewById(R.id.input1);
-        EditText input2 = findViewById(R.id.input2);
-        TextView operationText = findViewById(R.id.operationText);
-        TextView resultText = findViewById(R.id.resultText);
-        Button addBtn = findViewById(R.id.addBtn);
-        Button subtractBtn = findViewById(R.id.subtractBtn);
-        Button divideBtn = findViewById(R.id.divideBtn);
-        Button multiplyBtn = findViewById(R.id.multiplyBtn);
-        Button calculateBtn = findViewById(R.id.calculateBtn);
+        txtScreen = findViewById(R.id.txtScreen);
+        setNumerics();
+        setOperators();
+        setKeys();
+    }
 
-        addBtn.setOnClickListener(new View.OnClickListener() {
+    private void setNumerics(){
+        View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                operationText.setText("+");
-                operation = "add";
-            }
-        });
-
-        subtractBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                operationText.setText("-");
-                operation = "subtract";
-            }
-        });
-
-        multiplyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                operationText.setText("X");
-                operation = "multiply";
-            }
-        });
-
-        divideBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                operationText.setText("/");
-                operation = "divide";
-            }
-        });
-
-        calculateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String num1 = input1.getText().toString();
-                String num2 = input2.getText().toString();
-                if (num1.isEmpty() || num2.isEmpty()) {
-                    Toast.makeText(getApplicationContext(),"Enter Values first",Toast.LENGTH_SHORT).show();
-                }
-                else if(operation == ""){
-                    Toast.makeText(getApplicationContext(),"Choose an operation",Toast.LENGTH_SHORT).show();
+                if(result != "")
+                {
+                    clear();
                 }
                 else {
-                    double a = Double.parseDouble(num1);
-                    double b = Double.parseDouble(num2);
-                    switch (operation){
-                        case "add":
-                            answer = a+b;
-                            break;
-                        case "subtract":
-                            answer = a-b;
-                            break;
-                        case "multiply":
-                            answer = a*b;
-                            break;
-                        case "divide":
-                            answer = a/b;
-                            break;
-                    }
-                    Toast.makeText(getApplicationContext(),"Result is " + String.valueOf(answer),Toast.LENGTH_SHORT).show();
-                    resultText.setText(String.valueOf(answer));
-                    answer = 0;
+                    Button button = (Button) v;
+                    txtScreen.setText(txtScreen.getText() + "" + (button.getText()));
                 }
             }
-        });
+        };
+        for (int id : numericButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void setOperators(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(result != "")
+                {
+                    clear();
+                }
+                else if(operatorSelected == false && txtScreen.getText().toString() != "") {
+                    operand1 = Double.parseDouble(txtScreen.getText().toString());
+                    Button button = (Button) v;
+                    txtScreen.setText(txtScreen.getText() + " " + (button.getText()) + " ");
+                    operatorSelected = true;
+                }
+                else{
+                    if(operatorSelected) {
+                        Toast.makeText(getApplicationContext(), "You have already selected an operator", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Please enter an operand first", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        };
+        for (int id : operatorButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void setKeys(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(result != "")
+                {
+                    clear();
+                }
+                else {
+                    Button button = (Button) v;
+                    switch (button.getText().toString()) {
+                        case "Delete":
+                            delete();
+                            break;
+                        case "=":
+                            calculate();
+                            break;
+                        case "C":
+                            clear();
+                    }
+                }
+            }
+        };
+        for (int id : keyButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void calculate(){
+        String[] arrOfTxtScreen = txtScreen.getText().toString().split(" ");
+        if(arrOfTxtScreen.length < 3){
+            Toast.makeText(getApplicationContext(), "Please enter operands and operators first", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            operand2 = Double.parseDouble(arrOfTxtScreen[2]);
+            switch (arrOfTxtScreen[1]){
+                case "+":
+                    add();
+                    break;
+                case "-":
+                    subtract();
+                    break;
+                case "*":
+                    multiply();
+                    break;
+                case "/":
+                    divide();
+                    break;
+            }
+            txtScreen.setText(txtScreen.getText() + " = " + result);
+            Toast.makeText(getApplicationContext(),"Result is " + result,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void add(){
+        result = String.valueOf(operand1 + operand2);
+    }
+
+    private void subtract(){
+        result = String.valueOf(operand1 - operand2);
+    }
+
+    private void multiply(){
+        result = String.valueOf(operand1 * operand2);
+    }
+
+    private void divide(){
+        result = String.valueOf(operand1 / operand2);
+    }
+
+    private void clear(){
+        txtScreen.setText("");
+        operatorSelected = false;
+        operand2 = 0.0d;
+        operand1 = 0.0d;
+        result = "";
+        Toast.makeText(getApplicationContext(),"Cleared, please enter values to continue",Toast.LENGTH_SHORT).show();
+    }
+
+    private void delete(){
+        String[] arrOfTxtScreen = txtScreen.getText().toString().split(" ");
+        System.out.println(Arrays.toString(arrOfTxtScreen));
+        switch (arrOfTxtScreen.length){
+            case 3:
+                txtScreen.setText(txtScreen.getText().toString().substring(0, txtScreen.getText().toString().length() - 1));
+                break;
+            case 2:
+                txtScreen.setText(txtScreen.getText().toString().substring(0, txtScreen.getText().toString().length() - 3));
+                operatorSelected = false;
+                break;
+            case 1:
+                if(txtScreen.getText().toString() != ""){
+                    txtScreen.setText(txtScreen.getText().toString().substring(0, txtScreen.getText().toString().length() - 1));
+                    operand1 = 0.0d;
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Nothing to delete",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
